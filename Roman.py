c0ed8ce29bf6591625ce9a624ca102932a2ffe8b
Ramaraja <r.ramaraja@yahoo.com>
2023-03-03 18:35:28 -0500

    Add convertIntegerToRoman

---
+++ Roman.py
@@ -0,0 +1,28 @@
+# Python3 program to convert
+# integer value to roman values
+
+def convertIntegerToRoman(number):
+    """Function to convert integer to Roman Numerals
+    Author: Ramaraja Ramanujan
+    IDE: VS Code
+    """
+    num = [1, 4, 5, 9, 10, 40, 50, 90,
+           100, 400, 500, 900, 1000]
+    sym = ["I", "IV", "V", "IX", "X", "XL",
+           "L", "XC", "C", "CD", "D", "CM", "M"]
+    i = 12
+
+    while number:
+        div = number // num[i]
+        number %= num[i]
+
+        while div:
+            print(sym[i], end="")
+            div -= 1
+        i -= 1
+
+
+if __name__ == "__main__":
+    number = 3549
+    print("Roman value is:", end=" ")
+    convertIntegerToRoman(number)
